7ad1e420822c71d3cc40176daeebbc010256d102
jbayer44 <88685897+jbayer44@users.noreply.github.com>
2024-12-03 05:22:22 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -55,7 +55,7 @@ class App {
                 reject(error);
             });
             client.on('warn', console.log);
-            echo env.DISCORD_TOKEN;
+            console.log("Disc token: " + process.env.DISCORD_TOKEN;
             client.login(process.env.DISCORD_TOKEN || '');
         });
     }
